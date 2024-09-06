dbe9962a0cdc3b5751c0fcc33e7ee6eb7a02bc00
stopmalwareservice45 <stopmalwareservice@duck.com>
2024-09-06 23:23:17 +0300

    Update proxy_file.php

--- proxy_file.php
+++ proxy_file.php
@@ -18,10 +18,23 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 // Выполнить запрос и получить ответ
 $response = curl_exec($ch);
 
+// Получить HTTP-код ответа
+$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
 // Закрыть cURL-сессию
 curl_close($ch);
 
-// Вывести ответ в качестве ответа прокси-сервера
-echo $response;
-
-?>
+// Проверить, был ли запрос успешным
+if ($http_code === 200) {
+    // Определить заголовки для передачи файла
+        header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename="' . basename($url) . '"');
+                header('Content-Length: ' . strlen($response));
+                    
+                        // Отправить содержимое файла
+                            echo $response;
+                            } else {
+                                // Если не удалось получить файл, вывести сообщение об ошибке
+                                    echo 'Ошибка: Не удалось получить файл. Код ответа: ' . $http_code;
+                                    }
+                                    ?>
